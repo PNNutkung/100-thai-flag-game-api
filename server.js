57ba28bc5b473224abf07c8d89e22ef45dcdba3f
PNNutkung <pipatpol.t@ku.th>
2017-01-16 11:36:45 +0700

    Add starter code

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const port = process.env.PORT || 18080;
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, project_id, Authorization, o-Requested-With");
+  next();
+});
+
+app.use(morgan('dev'))
+
+app.listen(port);
+console.log('100 Thai Flag Game API online!');
