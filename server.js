dea45197f8a0416de79df04adb6b49d81bdfe0ff
PNNutkung <pipatpol.t@ku.th>
2017-01-26 11:56:46 +0700

    Add write to log file

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const FileStreamRotator = require('file-stream-rotator');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const config = require('./config');
+const fs = require('fs');
+const path = require('path');
 const app = express();
-const config = require('./config')
+const logDirectory = path.join(__dirname, 'log');
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+const accessLogStream = FileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: path.join(logDirectory, 'access-%DATE%.log'),
+  frequency: 'daily',
+  verbose: false
+});
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -14,6 +27,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, project_id, Authorization, o-Requested-With");
   next();
 });
-app.use(morgan('common'))
+app.use(morgan('combined', {stream: accessLogStream}));
 
 require('./routes/routesManager')(app, express);
